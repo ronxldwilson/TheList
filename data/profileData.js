e39c6eda518a09286d2c5f6df4a11e916fe0cd3b
RonxldWilson <ronaldwilson00000@gmail.com>
2024-12-27 18:53:37 +0530

    Changed profile data for Proxima Mumbai

--- data/profileData.js
+++ data/profileData.js
@@ -16,7 +16,7 @@ const profileData = [
 
       Our core hypothesis is that computation will shift from GPUs to custom ASICs-based modern AI chips. ( Rather it is happening as Google ditched NVIDIA GPUs a long time ago with TPU & AWS launched Trainium chips recently, as well as modern chips like Groq & Cerebras beating the Benchmarks). Therefore InTacht aims to facilitate the creation of custom clusters that combine ASICs, FPGAs, and modern AI chips, or offer them through a shared cloud.
       `,
-            how_will_funds_be_used: `1)Legal (Registering Delaware C Corp & basic compliance fees) <br>
+      how_will_funds_be_used: `1)Legal (Registering Delaware C Corp & basic compliance fees) <br>
       2)Saas Billing
       <br>
       3)Living expenses (assuming 3 months runway) which include Rent & other:- 
@@ -27,7 +27,7 @@ const profileData = [
         'I’m Utkrisht, a 19 Y/o college dropped out from Lucknow, India.',
       funding_link: '',
       socialLinks: {
-        twitter: 'https://x.com/koq_utx',
+        twitter: 'https://x.com/koq_utx'
       },
       money_recieved_till_today: ''
     }
@@ -37,31 +37,67 @@ const profileData = [
     slug: 'akshay-kripalani',
     name: 'Akshay Kripalani',
     buildingInfo: `Building Proxima Mumbai`,
-    cardSubContent: `Proxima is a Mumbai based hyper-curated community of builders and founders alike. We host hackathons, demo days, and meetups with VCs`,
+    cardSubContent: `Proxima is a Mumbai based hyper-curated community of builders and founders alike. We host hackathons, demo days and co-working meetups. We are currently 50+ members strong with talented founders in the community, like Armaan Dhanda, Harsh Agarwal, and Dhravya Shah.`,
     bannerImage: '/images/akshay-kripalani/bannerImage.jpg',
     profileImage: '/images/akshay-kripalani/personImage.jpg',
     data_added: 'Lorem Ipsum',
     pageContent: {
-      about_the_product:
-        'Proxima hosts bi-monthly co-working meetups where members showcase what they are building, and also build using new tech that is availability ',
-      how_will_funds_be_used:
-        `Secure better co-working spaces
-        <br>
-        <br>
-        Access to more advanced AI models for our in-house AI model that is current under development.
-        <br>
-        <br>
-        Marketing and distribution 
+      about_the_product: `
+      Proxima hosts bi-monthly co-working meetups where members showcase what they are building, and also build using new tech that is availability. We also host demo days, where members showcase what they have been working on, and hackathons. We are currently building Torch: An AI-Powered people search engine, to find the highest quality of builders and founders, with plans to scale it up in the future. 
+      <br>
+      <br>
+<ul>
+  <li>
+    <strong>Access to top industry leaders:</strong> Lead engineers in the field of Artificial Intelligence, Machine Learning, and DevOps are part of our group.
+  </li>
+  <li>
+    <strong>Insights with founders:</strong> Get to know the most talented founders and how they operate, exchange insights, and grow your network.
+  </li>
+  <li>
+    <strong>Stay up to date with the latest tech news:</strong> Our group often discusses new or niche tools that come out in the field of software, and you get to find these first.
+  </li>
+</ul>
+
+<br>
+We have also hosted Dawn Horizon Summit, a showcase event for DawnAR - A start-up building use-case centric AR glasses. 
+      `,
+      how_will_funds_be_used: `
+      <ul>
+  <li>
+    <strong>Secure better co-working spaces:</strong> A dedicated co-working space for our meetups allows us to host bigger events and better hackathons. Having access to such spaces would be greatly beneficial for us.
+  </li>
+  <li>
+    <strong>Access to better software:</strong> Torch, our in-house AI-powered people search engine, is under active development. If we had access to paid AI models, it would allow us to make our product even better.
+  </li>
+  <li>
+    <strong>Marketing and distribution:</strong> Having access to funds for marketing lets us grow our reach. Currently, we have 140+ followers on Twitter, but with access to Twitter Blue, our reach would grow much more.
+  </li>
+</ul>
+ 
 `,
-      about_them:
-        'The founding members are all 19 and based in Mumbai, India. ',
+      about_them: `<h2 style="font-size: 30px">Co-Founders:</h2>
+  <p>
+    <strong>Yash Panditrao</strong> and <strong>Manthan Maheshwari</strong>: Co-Founders at 
+    <a href="https://twitter.com/RaiseGate" target="_blank" style="color: blue;">@RaiseGate</a>. Bridging the gap between early-stage startups and venture capital firms with data-driven tools.
+  </p>
+  
+  <h2 style="font-size: 30px">Team Members:</h2>
+  <ul>
+    <li>
+      <strong>Shlok Parab:</strong> Lead web developer at Torch.
+    </li>
+    <li>
+      <strong>Akshay Kripalani:</strong> Lead AI developer at Torch.
+    </li>
+  </ul>`,
       funding_link: '',
       socialLinks: {
-        twitter: 'https://x.com/akshay_krips',
+        twitter: 'https://x.com/akshay_krips'
       },
       money_recieved_till_today: ''
     }
-  },{
+  },
+  {
     key: 3,
     slug: 'prashant-sharma',
     name: 'Prashant Sharma',
@@ -71,15 +107,12 @@ const profileData = [
     profileImage: '/images/prashant-sharma/personImage.jpeg',
     data_added: '23-12-2024',
     pageContent: {
-      about_the_product:
-        `I’m creating a platform where people can learn skills that the education system misses, such as tech, personal finance, and personal branding—skills people are passionate about. We offer mentorship from top experts, not just courses. More importantly, we’re also creating a community where they can execute ideas and grow together. I started this because I gained many skills at a young age but lacked the support and environment needed, and I want to provide that for others.`,
-      how_will_funds_be_used:
-        `The money will be used to build and improve the platform, making it a more effective place for students and mentors to connect. A portion will be used to onboard both students and mentors to grow our community. Marketing will help us reach a larger audience, attracting more users to the platform. Team salaries will ensure we have the right people to manage the platform, develop features, and support users. Office expenses will cover the costs of running our day-to-day operations efficiently, ensuring the business runs smoothly`,
-      about_them:
-        `I'm 17 years old high school dropout from Chennai, Tamil Nadu (India) and my home town is Jaipur, Rajasthan (India).`,
+      about_the_product: `I’m creating a platform where people can learn skills that the education system misses, such as tech, personal finance, and personal branding—skills people are passionate about. We offer mentorship from top experts, not just courses. More importantly, we’re also creating a community where they can execute ideas and grow together. I started this because I gained many skills at a young age but lacked the support and environment needed, and I want to provide that for others.`,
+      how_will_funds_be_used: `The money will be used to build and improve the platform, making it a more effective place for students and mentors to connect. A portion will be used to onboard both students and mentors to grow our community. Marketing will help us reach a larger audience, attracting more users to the platform. Team salaries will ensure we have the right people to manage the platform, develop features, and support users. Office expenses will cover the costs of running our day-to-day operations efficiently, ensuring the business runs smoothly`,
+      about_them: `I'm 17 years old high school dropout from Chennai, Tamil Nadu (India) and my home town is Jaipur, Rajasthan (India).`,
       funding_link: 'https://futuresociety.gumroad.com/l/cobee',
       socialLinks: {
-        twitter: 'https://x.com/drop_prashant',
+        twitter: 'https://x.com/drop_prashant'
       },
       money_recieved_till_today: ''
     }
